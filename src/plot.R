d8d32bc28f47039b1c67b658af5bc028e582cdfb
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2019-04-04 16:19:35 +0100

    update

---
+++ src/plot.R
@@ -0,0 +1,7 @@
+#Have a look at the plots
+p <- ggplot(data, aes(y=y,x=x))
+p+geom_point(aes(x,y), size = 2, colour="#CC0000")+stat_smooth(method = "lm", formula = y ~ x + I(x^2), size = 1)+labs(title="Quadratic regression model", x ="Number of telephone calls", y = "Duration of telephone calls",subtitle = "Model fitted wiht a quadratic approximation")
+
+ggsave("fig/quadraticregression.png", width = 10, height = 8)
+
+reg <- lm(y ~ x + I(x^2))
